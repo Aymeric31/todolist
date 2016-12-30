d0bd6eb6989cf30dc91e741179859e138517deda
Aymeric <aymeric.gioia@hotmail.fr>
2016-12-30 22:14:15 +0100

    initial commit

---
+++ java.js
@@ -0,0 +1,29 @@
+(function(){
+
+  var todo = document.querySelector( '#todolist' ),
+      form = document.querySelector( 'form' ),
+      field = document.querySelector( '#newitem' );
+    
+  form.addEventListener( 'submit', function( ev ) {
+    var text = field.value;
+    if ( text !== '' ) {
+      todo.innerHTML += '<li>' + text + '</li>';
+      field.value = '';
+      field.focus();
+    }
+    ev.preventDefault();
+  }, false);
+
+  todo.addEventListener( 'click', function( ev ) {
+    var t = ev.target;
+    if ( t.tagName === 'LI' ) {
+      if ( t.classList.contains( 'done' ) ) {
+        t.parentNode.removeChild( t );
+      } else {  
+        t.classList.add( 'done' );
+      }
+    };
+    ev.preventDefault();
+  }, false);
+
+})();
